05cf73044be8c4ad14a448875e660b941bd422ba
Alexey Novichikhin <32311883+NovichikhinAlexey@users.noreply.github.com>
2020-07-31 13:48:32 +0300

    Update Program.cs

--- src/SimpleLP/Program.cs
+++ src/SimpleLP/Program.cs
@@ -11,28 +11,37 @@ namespace SimpleLP
     {
         static async Task Main(string[] args)
         {
+            // connect to API
             var key = Environment.GetEnvironmentVariable("HFT_API_KEY");
             var api = new ApiTrader("https://hft-apiv2-grpc.lykke.com", key);
 
+            // create a bot with BTCUSD pair
             var trader = new MarketManager(api, "BTCUSD");
 
+            // init trading bot: Current Price = 10950, Limit Order size = 0.0001 BTC, Delta(level spread) = 10 USD, count level per side = 15            
             trader.ResetMarke(10950, 0.0001m, 10, 15);
+            
+            // place Limit Orders
             await trader.PlaceToMarketAsync();
 
             while (true)
             {
                 try
                 {
+                    // subscribe to trade stream
                     var tradeStream = api.Client.PrivateApi.GetTradeUpdates(new Empty());
 
+                    // wait next trade event
                     while (await tradeStream.ResponseStream.MoveNext())
                     {
+                        // handle each trade from event
                         foreach (var trade in tradeStream.ResponseStream.Current.Trades)
                         {
                             Console.WriteLine($"TRADE: orderId: {trade.OrderId}, size: {trade.BaseVolume}, role: {trade.Role}");
                             await trader.HandleTrade(trade.OrderId);
                         }
 
+                        // place new Limit Orders
                         await trader.PlaceToMarketAsync();
                     }
 
